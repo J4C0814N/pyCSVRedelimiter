9c1001737792779282a5a13529e614c8f2874dc0
J4C0814N <jacob@intrepidweb.com.au>
2017-05-09 09:39:47 +1000

    Updated CSVRedelimiter
    All CSVs in the current directory are fixed and saved to a sub directory
    added options to keep or remove quotes, and update the delimiters.

--- CSVRedelimiter.py
+++ CSVRedelimiter.py
@@ -1,28 +1,43 @@
 import os
+import glob
+path = "./*.csv"
 
+initialChar = ","
 replaceChar = "|"
+removeQuotes = True
+savePath = "./fixed/" #Directory currently needs to exist first
 
-with open("test.csv") as csvFile:
-    for line in csvFile:
-        #Split line in to a set of characters
-        lineList = list(line)
+for fname in glob.glob(path):
+	print(os.path.basename(fname))
+	with open(fname) as csvFile:
+		with open(savePath+fname, 'w') as fixedcsv:
+			for line in csvFile:
+				#Split line in to a set of characters
+				lineList = list(line)
 
-        notInQuotes = True #Because we will always start in an unquoted context.
+				notInQuotes = True #Because we will always start in an unquoted context.
 
-        charNum = len(lineList)
+				charNum = len(lineList)
 
-        i = 0
+				i = 0
 
-        while (i < charNum):
-            if lineList[i] == "," and notInQuotes:
-                lineList[i] = replaceChar
+				while (i < charNum):
+					if lineList[i] == initialChar and notInQuotes:
+						lineList[i] = replaceChar
 
-            if lineList[i] == '"':
-                if notInQuotes:
-                    notInQuotes = False
-                elif not notInQuotes:
-                    notInQuotes = True
+					if lineList[i] == '"':
+						if notInQuotes:
+							if removeQuotes:
+								lineList[i] = ''
+							notInQuotes = False
+						elif not notInQuotes:
+							if removeQuotes:
+								lineList[i] = ''
+							notInQuotes = True
 
-            i += 1
+					i += 1
 
-        print("".join(lineList))
+				print("".join(lineList))
+				fixedcsv.write("".join(lineList))
+
+	
