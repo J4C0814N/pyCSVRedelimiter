d76431c8587398c1c179995e9a4694d5f7cc90da
J4C0814N <jacob@intrepidweb.com.au>
2017-05-09 13:56:01 +1000

    Fixed multi-line quoted text issue

--- CSVRedelimiter.py
+++ CSVRedelimiter.py
@@ -1,4 +1,5 @@
 import os
+import csv
 import glob
 path = "./*.csv"
 
@@ -6,25 +7,28 @@ initialChar = ","
 replaceChar = "|"
 removeQuotes = True
 savePath = "./fixed/" #Directory currently needs to exist first
+tempString = "temp_"
 
 for fname in glob.glob(path):
-	print(os.path.basename(fname))
-	with open(fname) as csvFile:
+	print("Processing "+os.path.basename(fname))
+	#Remove any additional line breaks that appear in quoted text
+	tempFile = savePath+tempString+os.path.basename(fname)
+	with open(fname, "rb") as input, open(savePath+tempString+os.path.basename(fname), "wb") as output:
+		w = csv.writer(output)
+		for record in csv.reader(input):
+			w.writerow(tuple(s.replace("\r\n","") for s in record))
+		
+	with open(tempFile) as csvFile:
 		with open(savePath+fname, 'w') as fixedcsv:
 			for line in csvFile:
 				#Split line in to a set of characters
 				lineList = list(line)
-
 				notInQuotes = True #Because we will always start in an unquoted context.
-
 				charNum = len(lineList)
-
 				i = 0
-
 				while (i < charNum):
 					if lineList[i] == initialChar and notInQuotes:
 						lineList[i] = replaceChar
-
 					if lineList[i] == '"':
 						if notInQuotes:
 							if removeQuotes:
@@ -37,7 +41,6 @@ for fname in glob.glob(path):
 
 					i += 1
 
-				print("".join(lineList))
 				fixedcsv.write("".join(lineList))
-
-	
+	print(os.path.basename(fname)+" Done")
+	os.remove(tempFile)
