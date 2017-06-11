ac72463cb8364237cc236fbe921e799453819bea
rollinginsanity <reece.payne@gmail.com>
2017-06-11 12:07:30 +1000

    Cleaned up logic
    Removed the need for a temp file.
    Changed the logic to replace newlines in strings.

--- CSVRedelimiter.py
+++ CSVRedelimiter.py
@@ -1,58 +1,71 @@
 import os
 import csv
 import glob
-path = "./*.csv"
+import shutil
 
+
+
+
+path = "./*.csv"
 #Some defaults.
 initialChar = ","
 replaceChar = "|"
-removeQuotes = True
+removeQuotes = False
 savePath = "./fixed/" 
-tempString = "temp_"
 
 def main():
-
+	print("#### Checking output directory exists.")
 	create_save_dir(savePath)
-
+	print(glob.glob(path))
+	print("#### Checking for .csv files.")
 	for fname in glob.glob(path):
-		print("Processing "+os.path.basename(fname))
+		print("#### Got: "+fname)
+		print("#### Processing "+os.path.basename(fname))
 		#Remove any additional line breaks that appear in quoted text
-		tempFile = savePath+tempString+os.path.basename(fname)
-		with open(fname, "rb") as input, open(savePath+tempString+os.path.basename(fname), "wb") as output:
-			w = csv.writer(output)
-			for record in csv.reader(input):
-				w.writerow(tuple(s.replace("\r\n","") for s in record))
-			
-		with open(tempFile) as csvFile:
+
+		with open(fname) as csvFile:
 			with open(savePath+fname, 'w') as fixedcsv:
 				for line in csvFile:
+					#Remove newlines from quoted text:
+					cleanLine = line.replace("\n", "").replace("\r", "")
 					#Split line in to a set of characters
-					lineList = list(line)
-					notInQuotes = True #Because we will always start in an unquoted context.
-					charNum = len(lineList)
+					lineChars = list(cleanLine)
+					inQuotes = False #Because we will always start in an unquoted context.
+					charNum = len(lineChars)
 					i = 0
+					print(line)
+					print("".join(lineChars))
 					while (i < charNum):
-						if lineList[i] == initialChar and notInQuotes:
-							lineList[i] = replaceChar
-						if lineList[i] == '"':
-							if notInQuotes:
+						
+						if lineChars[i] == initialChar and not inQuotes:
+							print("#### Replacing delimiter")
+							lineChars[i] = replaceChar
+						if lineChars[i] == '"':
+							print("#### Found quotes")
+							if not inQuotes:
 								if removeQuotes:
-									lineList[i] = ''
-								notInQuotes = False
-							elif not notInQuotes:
+									lineChars[i] = ''
+								inQuotes = True
+							elif inQuotes:
 								if removeQuotes:
-									lineList[i] = ''
-								notInQuotes = True
+									lineChars[i] = ''
+								inQuotes = False
 
 						i += 1
 
-					fixedcsv.write("".join(lineList))
+					fixedcsv.write("".join(lineChars)+"\n")
 		print(os.path.basename(fname)+" Done")
-		os.remove(tempFile)
 
 #Create our save directory.
 def create_save_dir(savePath):
-	if not os.path.exists(savePath):
+
+	if os.path.exists(savePath):
+		print("#### Found exsiting directory "+savePath+", deleting all contents..")
+		shutil.rmtree(savePath)
+		os.makedirs(savePath)
+	else:
+		#No directory found.
+		print("#### Creating output directory.")
 		os.makedirs(savePath)
 
 if __name__ == '__main__':
